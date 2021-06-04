fb6ff9c7e0089b6ce11b859346e33527ce558021
dance2die <sungmkim80@gmail.com>
2021-06-04 16:14:14 -0400

    extended 'screens'

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,24 @@
+/**
+ * Material Design breakpoints
+ * @see https://material.io/design/layout/understanding-layout.html#layout-anatomy
+ * @see https://tailwindcss.com/docs/breakpoints
+ */
+const screens = {
+	sm: { min: '0', max: '599px' },
+	md: { min: '600px', max: '904px' },
+	lg: { min: '905px', max: '1239px' },
+	xl: { min: '1240px', max: '1439px' },
+	'2xl': { min: '1440px' }
+};
+
 module.exports = {
 	// mode: 'jit',
 	purge: [ './pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}' ],
 	darkMode: false, // or 'media' or 'class'
 	theme: {
-		extend: {}
+		extend: {
+			screens
+		}
 	},
 	variants: {
 		extend: {}
