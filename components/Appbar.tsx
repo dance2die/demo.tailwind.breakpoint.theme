b16ecb8d83612674340f8de847e02710acf50e98
dance2die <sungmkim80@gmail.com>
2021-06-04 21:36:07 -0400

    Created a theme color

--- components/Appbar.tsx
+++ components/Appbar.tsx
@@ -2,9 +2,47 @@ import Link from 'next/link';
 
 function Appbar() {
 	return (
-		<section>
-			<h1>This is an appbar!</h1>
-		</section>
+		<header className="mdc-top-app-bar">
+			<div className="mdc-top-app-bar__row">
+				<section className="mdc-top-app-bar__section mdc-top-app-bar__section--align-start">
+					<button className="h-3 w-3" aria-label="Open navigation menu">
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							className="h-1 w-1"
+							fill="none"
+							viewBox="0 0 24 24"
+							stroke="currentColor"
+						>
+							<path
+								strokeLinecap="round"
+								strokeLinejoin="round"
+								strokeWidth={2}
+								d="M4 6h16M4 12h16M4 18h16"
+							/>
+						</svg>
+						<p className="sr-only">Menu</p>
+					</button>
+					<span className="mdc-top-app-bar__title">Page title</span>
+				</section>
+				<section className="mdc-top-app-bar__section mdc-top-app-bar__section--align-end" role="toolbar">
+					<button
+						className="material-icons mdc-top-app-bar__action-item mdc-icon-button"
+						aria-label="Favorite"
+					>
+						favorite
+					</button>
+					<button className="material-icons mdc-top-app-bar__action-item mdc-icon-button" aria-label="Search">
+						search
+					</button>
+					<button
+						className="material-icons mdc-top-app-bar__action-item mdc-icon-button"
+						aria-label="Options"
+					>
+						more_vert
+					</button>
+				</section>
+			</div>
+		</header>
 	);
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -11,13 +11,37 @@ const screens = {
 	'2xl': { min: '1440px' }
 };
 
+/**
+ * Custom Material Design theme colors
+ * @see https://material.io/design/color/the-color-system.html#color-theme-creation
+ * @see https://tailwindcss.com/docs/customizing-colors#extending-the-defaults
+ * @see https://material.io/resources/color/#!/?view.left=1&view.right=1&primary.color=db1f5e&secondary.color=ffc600&primary.text.color=ffffff
+ */
+const colors = {
+	primary: '#db1f5d',
+	'primary-light': '#a30035',
+	'primary-dark': '#ff5e8b',
+	secondary: '#ffc600',
+	'secondary-light': '#fff94f',
+	'secondary-dark': '#c79600',
+	background: '#fff',
+	surface: '#fff',
+	error: '#b00020',
+	'on-primary': '#fff',
+	'on-secondary': '#000',
+	'on-background': '#000',
+	'on-surface': '#000',
+	'on-error': '#fff'
+};
+
 module.exports = {
 	mode: 'jit',
 	purge: [ './pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}' ],
 	darkMode: false, // or 'media' or 'class'
 	theme: {
 		extend: {
-			screens
+			screens,
+			colors
 		}
 	},
 	variants: {
